18c64766dddd0877f8e027b1e3480352e1786382
Boris Rybalkin <ribalkin@gmail.com>
2019-04-10 21:51:49 +0100

    upgrade to latest packaging tools and v1.1.1

--- build.sh
+++ build.sh
@@ -1,46 +1,36 @@
 #!/bin/bash -xe
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
 
-NAME=users
-LDAPCHERRY_VERSION=0.5.2
-DOWNLOAD_URL=http://artifact.syncloud.org/3rdparty
-
-if [[ -z "$1" ]]; then
-    echo "usage $0 version"
+if [[ -z "$2" ]]; then
+    echo "usage $0 app version"
     exit 1
 fi
 
+NAME=$1
+LDAPCHERRY_VERSION=1.1.1
+
 ARCH=$(uname -m)
-VERSION=$1
+VERSION=$2
 
-if [ -n "$DRONE" ]; then
-    echo "running under drone, removing coin cache"
-    rm -rf ${DIR}/.coin.cache
-fi
+DOWNLOAD_URL=http://artifact.syncloud.org/3rdparty
 
-cd ${DIR}
-
-rm -rf build
+rm -rf ${DIR}/build
 BUILD_DIR=${DIR}/build/${NAME}
 mkdir -p ${BUILD_DIR}
 
 coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/nginx-${ARCH}.tar.gz
 coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/python-${ARCH}.tar.gz
-#mv ${BUILD_DIR}/python3 ${BUILD_DIR}/python
 ${BUILD_DIR}/python/bin/pip install -r ${DIR}/requirements.txt
 
-apt-get install libyaml-dev
+#apt-get install libyaml-dev
 
 #cp -r ${DIR}/bin ${BUILD_DIR}
 #cp -r ${DIR}/config ${BUILD_DIR}/config.templates
 #cp -r ${DIR}/hooks ${BUILD_DIR}
 
 cd ${BUILD_DIR}
-
-mkdir META
-echo ${NAME} >> META/app
-echo ${VERSION} >> META/version
 
 echo "getting latest diaspora source"
 wget --progress=dot:giga https://github.com/kakwa/ldapcherry/archive/${LDAPCHERRY_VERSION}.tar.gz
@@ -49,8 +39,12 @@ tar xzf ${LDAPCHERRY_VERSION}.tar.gz
 
 mv ldapcherry-${LDAPCHERRY_VERSION} ldapcherry
 cd ldapcherry
-sed -i 's/CherryPy >= 3.0.0/CherryPy >= 3.0.0, < 18.0.0/g' setup.py
+#sed -i 's/CherryPy >= 3.0.0/CherryPy >= 3.0.0, < 18.0.0/g' setup.py
 ${BUILD_DIR}/python/bin/python setup.py install
+
+mkdir ${DIR}/build/${NAME}/META
+echo ${NAME} >> ${DIR}/build/${NAME}/META/app
+echo ${VERSION} >> ${DIR}/build/${NAME}/META/version
 
 echo "snapping"
 SNAP_DIR=${DIR}/build/snap
@@ -64,4 +58,6 @@ echo "version: $VERSION" >> ${SNAP_DIR}/meta/snap.yaml
 echo "architectures:" >> ${SNAP_DIR}/meta/snap.yaml
 echo "- ${ARCH}" >> ${SNAP_DIR}/meta/snap.yaml
 
-mksquashfs ${SNAP_DIR} ${DIR}/${NAME}_${VERSION}_${ARCH}.snap -noappend -comp xz -no-xattrs -all-root
+PACKAGE=${NAME}_${VERSION}_${ARCH}.snap
+echo ${PACKAGE} > ${DIR}/package.name
+mksquashfs ${SNAP_DIR} ${DIR}/${PACKAGE} -noappend -comp xz -no-xattrs -all-root
