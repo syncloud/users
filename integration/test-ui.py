0ecf60433eb6860db22e620a2c0279fbdb15001f
Boris Rybalkin <ribalkin@gmail.com>
2019-05-22 19:46:14 +0100

    fix ui mode

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -15,11 +15,11 @@ screenshot_dir = join(DIR, 'screenshot')
 TMP_DIR = '/tmp/syncloud/ui'
 
 @pytest.fixture(scope="session")
-def module_setup(request, device, log_dir):
-    request.addfinalizer(lambda: module_teardown(device, log_dir))
+def module_setup(request, device, log_dir, ui_mode):
+    request.addfinalizer(lambda: module_teardown(device, log_dir, ui_mode))
 
 
-def module_teardown(device, log_dir):
+def module_teardown(device, log_dir, ui_mode):
     device.activated()
     device.run_ssh('mkdir -p {0}'.format(TMP_DIR), throw=False)
     device.run_ssh('journalctl > {0}/journalctl.ui.{1} log'.format(TMP_DIR, ui_mode), throw=False)
