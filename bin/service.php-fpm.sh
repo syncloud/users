36866b30386138eaaf55ea732ee69be0651864a1
Boris Rybalkin <ribalkin@gmail.com>
2022-04-07 18:56:29 +0300

    php

--- bin/service.php-fpm.sh
+++ bin/service.php-fpm.sh
@@ -2,5 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-export LD_LIBRARY_PATH=${DIR}/php/lib
-exec ${DIR}/php/lib/ld.so --library-path ${DIR}/php/lib:${DIR}/php/lib/private $DIR/php/sbin/php-fpm -y ${SNAP_DATA}/config/php-fpm.conf -c ${SNAP_DATA}/config/php.ini
+exec $DIR/php/bin/php-fpm.sh -y ${SNAP_DATA}/config/php-fpm.conf -c ${SNAP_DATA}/config/php.ini
