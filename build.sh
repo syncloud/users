d54dd7cecfff7d3aca091d2226b8f51334df7522
Boris Rybalkin <ribalkin@gmail.com>
2023-02-24 22:18:27 +0000

    try fork

--- build.sh
+++ build.sh
@@ -8,23 +8,8 @@ if [[ -z "$1" ]]; then
     exit 1
 fi
 
-VERSION=$1
-
-ARCH=$(uname -m)
-DOWNLOAD_URL=https://github.com/syncloud/3rdparty/releases/download/1
-
-apt update
-apt install -y wget
-
 BUILD_DIR=${DIR}/build/snap
-mkdir -p ${BUILD_DIR}
 
 cp -r ${DIR}/bin ${BUILD_DIR}
 cp -r ${DIR}/config ${BUILD_DIR}
 cp -r ${DIR}/hooks ${BUILD_DIR}
-
-cd ${DIR}/build
-
-wget --progress=dot:giga https://github.com/wheelybird/ldap-user-manager/archive/${VERSION}.tar.gz
-tar xzf ${VERSION}.tar.gz
-mv ldap-user-manager-${VERSION}/www ${BUILD_DIR}
